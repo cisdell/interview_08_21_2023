1f28d98103308247e25ea04ace8b9b73dc922e80
Andrew Cho <cisdell@gmail.com>
2023-08-24 17:50:18 -0400

    work at the lib

--- game.py
+++ game.py
@@ -8,17 +8,44 @@ def random_numbers():
     digits = [int(d) for d in str(num)]
     return digits
 
-test = random_numbers()
+def evaluate_attempt(myNums, userNums, turns):
+    turns += 1
+    if myNums == userNums:
+        return f'answer is correct. It took you {turns} turns'
+    else:
+        heads = 0
+        tails = 0
+        for i in range(myNums):
+            num1 = myNums[i]
+            num2 = userNums[i]
+            if num1 == num2:
+                heads += 1
+            else:
+                if num2 in myNums:
+                    tail += 1
+        return f"heads: {heads}, tails: {tails}"
+
+myNums, userNums = [1,2,3,4,5],[1,2,3,4,5]
+
+evaluate_attempt(myNums, userNums, turns)
+
+
+# test = random_numbers()
 
 # print(test)
+myNums = random_numbers()
 
-def game_play(game_state):
+def game_play(game_state, myNums):
     user_guess = input('put in a 5 digit number')
+
     while len(user_guess) != 5:
         user_guess = input('make sure you put 5 digit numbers')
+    userNums = [int(d) for d in str(user_guess)]
 
 
 
 
 
-game_play(game_state)
+
+
+# game_play(game_state)
